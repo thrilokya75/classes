4c193005db2115e09005eaa6ae4a37c9a6813f9e
thrilokya75 <58476769+thrilokya75@users.noreply.github.com>
2019-12-05 17:05:42 +0530

    Add files via upload

---
+++ Caluclate.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+class Caluclator
+{
+ int power(int num1,int num2)
+{
+ double p=Math.pow(num1,num2);
+ return p;
+}
+double power(double num1,double num2)
+{
+ q=Math.pow(num1,num2);
+ return q;
+}
+}
+class Caluclate
+{
+public static void main(String args[])
+{
+Caluclator.power(10,2);
+Caluclator.power(5.2,2);
+System.out.println(p);
+System.out.println(q);
+}
+} 
